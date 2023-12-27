5d95ffc167713ed4bad0925b7dd10412680b311e
Elshaman <cbuitragoo.2023@gmail.com>
2023-12-27 08:52:13 -0500

    7_anotaciones_postconstruct_predestroy

--- src/main/java/org/shaman/springboot/di/factura/springbootdifactura/models/Invoice.java
+++ src/main/java/org/shaman/springboot/di/factura/springbootdifactura/models/Invoice.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 public class Invoice {
 
@@ -19,6 +22,33 @@ public class Invoice {
     @Autowired
     @Qualifier("default")
     private List<Item> items;
+
+
+    //el postconstructor se ejecuta cuando
+    // - se haya instanciado
+    // - se hayan inyectado todos los valores
+    @PostConstruct
+    public void init(){
+        System.out.println("creando el componente de la factura");
+        //System.out.println(this.customer.getFirstname() + " " + this.customer.getLastname());
+        //System.out.println(this.description);
+        this.customer.setFirstname(customer.getFirstname().concat(" dios negro de merak")); 
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("desrtuyendo el componente");
+    }
+
+    /*
+    Recien en el constructor aun no se han inyectado los 
+    valores con autowired
+    */
+    public Invoice() {
+        System.out.println("creando el componente de la factura");
+        System.out.println(this.customer);
+        System.out.println(this.description);
+    }
 
     public Customer getCustomer() {
         return customer;
