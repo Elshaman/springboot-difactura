ce860018d596d56e4224a9a0e2c7b4be3e990021
Elshaman <cbuitragoo.2023@gmail.com>
2023-12-27 08:21:35 -0500

    multiples lineas de factura

--- src/main/java/org/shaman/springboot/di/factura/springbootdifactura/Appconfig.java
+++ src/main/java/org/shaman/springboot/di/factura/springbootdifactura/Appconfig.java
@@ -7,6 +7,7 @@ import org.shaman.springboot.di.factura.springbootdifactura.models.Item;
 import org.shaman.springboot.di.factura.springbootdifactura.models.Product;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
@@ -21,8 +22,20 @@ public class Appconfig {
             new Item (p1 , 2) , 
             new Item (p2 , 4)
         );
+    }
 
-
+     @Bean("default")
+    List<Item> itemsInvoiceOficina(){
+        Product p1 = new Product("Monitor Asus 24", 700);
+        Product p2 = new Product("NoteBook Razer", 2400);
+        Product p3 = new Product("Impresora Hp", 800);
+        Product p4 = new Product("Escritorio Oficina", 900);
+        return Arrays.asList(
+            new Item (p1 , 4) , 
+            new Item (p2 , 6) ,
+            new Item (p3 , 4) ,
+            new Item (p4 , 1) 
+        );
     }
 
 }
--- src/main/java/org/shaman/springboot/di/factura/springbootdifactura/models/Invoice.java
+++ src/main/java/org/shaman/springboot/di/factura/springbootdifactura/models/Invoice.java
@@ -3,6 +3,7 @@ package org.shaman.springboot.di.factura.springbootdifactura.models;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -16,32 +17,43 @@ public class Invoice {
     private String description;
 
     @Autowired
+    @Qualifier("default")
     private List<Item> items;
-
 
     public Customer getCustomer() {
         return customer;
     }
+
     public void setCustomer(Customer customer) {
         this.customer = customer;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public List<Item> getItems() {
         return items;
     }
+
     public void setItems(List<Item> items) {
         this.items = items;
     }
 
-    public int getTotal(){
-        return 0;
-    }
+    public int getTotal() {
+        //int total = 0;
+        /*for (Item item : this.items) {
+            total += item.getImporte();
+        }*/
+        int total = items.stream()
+                    .map(item -> item.getImporte())
+                    .reduce(0, (sum , importe)->sum + importe);
 
-    
+        return total;
+    }
 
 }
